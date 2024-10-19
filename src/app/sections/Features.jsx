170d2a4cd9510f8de8d6b45e09998ed171f6c054
Amirhossein-Chapari <amirhossein.chapari80@gmail.com>
2024-10-19 12:03:51 +0330

    sth

--- src/app/sections/Features.jsx
+++ src/app/sections/Features.jsx
@@ -52,14 +52,14 @@ function Features(course) {
                                     keyframes={bottomToTopAnimation}
                                     delay={i * 100}
                                 >
-                                    <div className="highlighted-box w-72 bg-[#84f5f5] p-3 rounded-2xl text-center relative shadow-[0_8px_16px_#98ADBD,0_4px_8px_#84f5f5] transition-shadow duration-200 hover:shadow-[0_12px_24px_rgba(0,0,0,0.5),0_6px_12px_#84f5f5]">
+                                    <div className="highlighted-box w-72 bg-[#84f5f5] p-3 rounded-[2rem] text-center relative shadow-[0_8px_16px_#98ADBD,0_4px_8px_#84f5f5] transition-shadow duration-200 hover:shadow-[0_12px_24px_rgba(0,0,0,0.5),0_6px_12px_#84f5f5]">
                                         <div className="circle flex items-center justify-center w-[110px] h-[110px] bg-[#f4f3ee] rounded-full absolute -top-[60px] left-1/2 transform -translate-x-1/2 shadow-[0_8px_16px_#98ADBD,0_4px_8px_#84f5f5] transition-shadow duration-200 z-20 hover:shadow-[0_12px_24px_rgba(0,0,0,0.5),0_6px_12px_#84f5f5]">
                                             <div className="innerCircle w-[90px] h-[90px] bg-[#84f5f5] rounded-full mx-auto relative shadow-[0_8px_16px_#98ADBD,0_4px_8px_#84f5f5] border-[5px] border-[#84f5f5] z-30 overflow-hidden">
                                                 <Image src={infoGif} />
                                             </div>
                                         </div>
                                         <div className="main">
-                                            <div className="innerBox relative bg-[#f4f3ee] rounded-2xl mx-auto text-center leading-8 text-sm shadow-[0_-8px_16px_#98ADBD,-8px_8px_16px_#98ADBD,8px_8px_16px_#98ADBD] transition-shadow duration-200 z-10">
+                                            <div className="innerBox relative bg-[#f4f3ee] rounded-[2rem] mx-auto text-center leading-8 text-sm shadow-[0_-8px_16px_#98ADBD,-8px_8px_16px_#98ADBD,8px_8px_16px_#98ADBD] transition-shadow duration-200 z-10">
                                                 <p className="translate-y-10 font-DanaMedium min-h-72 p-2">
                                                     {attribute.attribute}
                                                 </p>
